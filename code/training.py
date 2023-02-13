1672ac8acafeebfd83347001b5c156f07e6becff
Mike Adams <mikethebos@icloud.com>
2023-02-13 13:59:24 -0700

    trim hyperparam search

--- code/training.py
+++ code/training.py
@@ -3,6 +3,7 @@ Script to train Random Forest
 """
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 from utils import entropy, gini, misclassification_error, get_train_val_split, get_accuracy
 from Forest import RandomForest
 from Results import Results
@@ -12,14 +13,14 @@ from Results import Results
 def main():
     pth = "../data/agaricus-lepiota-training.csv"
     df = pd.read_csv(pth)
-    split_seeds = np.arange(1, 11, 1)
+    split_seeds = np.arange(1, 4, 1)
     feat_rs = np.arange(0.1, 0.8, 0.1)
     metrics = {"entropy": entropy, "gini": gini, "misclassification_error": misclassification_error}
-    alphas = [0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 0.99]
+    alphas = [0.05]
     bag_rs = np.arange(0.1, 0.8, 0.1)
-    tree_counts = np.arange(10, 110, 10)
-    seeds_for_bag_seed_generator = np.arange(1, 11, 1)
-    seeds_for_feat_bag_seed_generator = np.arange(1, 11, 1)
+    tree_counts = np.arange(10, 20, 10)
+    seeds_for_bag_seed_generator = np.arange(1, 4, 1)
+    seeds_for_feat_bag_seed_generator = np.arange(1, 4, 1)
     
     """
     Parameter order for results: "feature_r", "metric_fn", "alpha", "bag_r", "tree_count",
@@ -28,6 +29,8 @@ def main():
                      'seed_for_feat_bag_seed_generator'
     """
     res = Results()
+
+    pbar = tqdm(total=len(split_seeds) * len(seeds_for_bag_seed_generator) * len(seeds_for_feat_bag_seed_generator) * len(tree_counts) * len(bag_rs) * len(feat_rs) * len(alphas) * len(metrics))
 
     for split_seed in split_seeds:
         train_df, val_df = get_train_val_split(df, val_r=0.2, seed=split_seed)
@@ -48,8 +51,11 @@ def main():
                                     avg_depth = forest.avg_tree_depth()
                                     max_depth = forest.max_tree_depth()
                                     res.add_entry([feat_r, metric, alpha, bag_r, tree_count, split_seed, max_depth, avg_depth, acc, bag_seed, feat_bag_seed])
+                                    pbar.update(1)
 
-    res.results_df.to_csv("../data/grid_search1.csv")
+    pbar.close()
+
+    res.results_df.to_csv("../data/grid_searchmike.csv")
 
 if __name__ == "__main__":
     main()
