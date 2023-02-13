785e4f7d25490a9e6972fb83ec381ec7f1e8181b
John Allen Ringer <johnjack0987@gmail.com>
2023-02-13 15:56:16 -0700

    Add script for inspecting results csv

---
+++ code/evaluation.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+
+"""
+@author Jack Ringer, Mike Adams
+Date:2/13/2023
+Description: Script to show which params gave the best (validation) accuracy
+"""
+
+
+def main(csv_path: str, acc_col: str = "accuracy"):
+    results_df = pd.read_csv(csv_path)
+    print("All results:")
+    print(results_df)
+    print("-" * 80)
+    best_idx = np.argmax(results_df[acc_col])
+    worst_idx = np.argmin(results_df[acc_col])
+    print(set(results_df[acc_col]))
+    print("Best params found were:", results_df.iloc[best_idx])
+    print("-" * 80)
+    print("Worst params found were:", results_df.iloc[worst_idx])
+
+
+if __name__ == "__main__":
+    results_pth = "../data/test_jack.csv"
+    main(results_pth)
