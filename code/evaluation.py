48010e8de86086f2cc8ce7e866398d65ea78ba6c
Mike Adams <mikethebos@icloud.com>
2023-02-15 16:43:05 -0700

    more detailed eval

--- code/evaluation.py
+++ code/evaluation.py
@@ -20,7 +20,30 @@ def main(csv_path: str, acc_col: str = "accuracy"):
     print("-" * 80)
     print("Worst params found were:", results_df.iloc[worst_idx])
 
+    print("metric fn avg acc ", results_df.groupby(["metric_fn"])["accuracy"].mean())
+    print("metric fn min acc ", results_df.groupby(["metric_fn"])["accuracy"].min())
+    print("metric fn max acc ", results_df.groupby(["metric_fn"])["accuracy"].max())
+
+    print("bag_r avg acc ", results_df.groupby(["bag_r"])["accuracy"].mean())
+    print("bag_r min acc ", results_df.groupby(["bag_r"])["accuracy"].min())
+    print("bag_r max acc ", results_df.groupby(["bag_r"])["accuracy"].max())
+
+    print("feature_r avg acc ", results_df.groupby(["feature_r"])["accuracy"].mean())
+    print("feature_r min acc ", results_df.groupby(["feature_r"])["accuracy"].min())
+    print("feature_r max acc ", results_df.groupby(["feature_r"])["accuracy"].max())
+
+    print("top_feature avg acc ", results_df.groupby(["top_feature"])["accuracy"].mean())
+    print("top_feature min acc ", results_df.groupby(["top_feature"])["accuracy"].min())
+    print("top_feature max acc ", results_df.groupby(["top_feature"])["accuracy"].max())
+
+    print("max_depth avg acc ", results_df.groupby(["max_depth"])["accuracy"].mean())
+    print("max_depth min acc ", results_df.groupby(["max_depth"])["accuracy"].min())
+    print("max_depth max acc ", results_df.groupby(["max_depth"])["accuracy"].max())
+
+    print("alpha avg acc ", results_df.groupby(["alpha"])["accuracy"].mean())
+    print("alpha min acc ", results_df.groupby(["alpha"])["accuracy"].min())
+    print("alpha max acc ", results_df.groupby(["alpha"])["accuracy"].max())
 
 if __name__ == "__main__":
-    results_pth = "../data/grid_searchmike.csv"
+    results_pth = "../data/grid_searchmike_4hr_val_r_99.csv"
     main(results_pth)
